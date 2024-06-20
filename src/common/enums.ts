677a3b60b41273616113d1d820b58200f528e7b6
eva <eva.pincin@gmail.com>
2024-06-20 12:55:54 +0200

    Created enums from db

---
+++ src/common/enums.ts
@@ -0,0 +1,28 @@
+export enum UserRole {
+  ADMIN = 'ADMIN',
+  USER = 'USER',
+  DENNIED = 'DENNIED',
+  PENDING = 'PENDING',
+}
+
+export enum TransactionType {
+  INCOME = 'INCOME',
+  EXPENSE = 'EXPENSE',
+}
+
+export enum InviteStatus {
+  PENDING = 'PENDING',
+  ACCEPTED = 'ACCEPTED',
+  EXPIRED = 'EXPIRED',
+}
+
+export enum ActionStatus {
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+}
+
+export enum TransactionStatus {
+  PENDING = 'PENDING',
+  APPROVED = 'APPROVED',
+  REJECTED = 'REJECTED',
+}
