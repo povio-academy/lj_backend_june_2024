9fcd62a18db3a10dfc99cf926ff79ddd713d090b
Vito Drofenik <vito@Vitos-MBP.localdomain>
2024-06-20 15:11:33 +0200

    added the get single endpoint

--- src/data-api/categories/admin/dto/create-category.admin.body.dto.ts
+++ src/data-api/categories/admin/dto/create-category.admin.body.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
 
 export class CreateCategoryAdminBodyDto {
     @IsString()
     @IsNotEmpty()
+    @MaxLength(50)
     @ApiProperty({description: 'Category name', example: 'Groceries'})
     name: string;
 }
