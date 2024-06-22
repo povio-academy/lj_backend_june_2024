a06694cd1ff7b52db662cc0a009f5d2e29b2f9b2
AljazKnific <ak3114@student.uni-lj.si>
2024-06-22 23:00:03 +0200

    Changed and fixed code for login and register

--- src/data-api/auth/auth.controller.ts
+++ src/data-api/auth/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpCode, Injectable, Post } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
 import { RegisterReqDto } from "./dto/register.req.dto";
 import { LoginReqDto } from "./dto/login.req.dto";
 import { JwtDto } from "./dto/Jwt.dto";
@@ -11,10 +11,17 @@ import { JwtDto } from "./dto/Jwt.dto";
 export class AuthController {
   constructor() {}
 
+  @ApiOperation({
+    summary: "Register a new user"
+  })
   @Post("/register")
   @HttpCode(204)
+  
   async register(@Body() body : RegisterReqDto) {}
 
+  @ApiOperation({
+    summary: "Login with an existing user"
+  })
   @Post("/login")
   async login(@Body() body : LoginReqDto) : Promise<JwtDto>  {
     return new JwtDto('token');
--- src/data-api/auth/dto/login.req.dto.ts
+++ src/data-api/auth/dto/login.req.dto.ts
@@ -1,15 +1,21 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, IsStrongPassword } from "class-validator";
 
 export class LoginReqDto {
-  @ApiProperty()
-  @Expose()
+  @ApiProperty({
+    description: "Users email",
+    example: "test@mail.com"
+  })
   @IsEmail()
+  @IsNotEmpty()
   email!: string;
 
-  @ApiProperty()
-  @Expose()
+  @ApiProperty({
+    description: "Users password",
+    example: "Password123!"
+  })
+  @IsNotEmpty()
   @IsString()
   password!: string;
 }
