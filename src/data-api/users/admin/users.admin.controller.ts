2b1a0b5ed985c4cf115c14034865ef87bd722fab
AljazKnific <ak3114@student.uni-lj.si>
2024-06-24 14:20:55 +0200

    Get all invited

--- src/data-api/users/admin/users.admin.controller.ts
+++ src/data-api/users/admin/users.admin.controller.ts
@@ -42,5 +42,10 @@ export class UsersAdminController {
         @Param('id') userId: string,
     ) {
         //delete and update user data
+    @ApiOperation({ summary: 'Get all invited users' })
+    @Get('/invites')
+    async getInvitedUsers(): Promise<UsersAdminResDto> {
+        //return users with a UserRole.PENDING
+        //add pagination
     }
 }
