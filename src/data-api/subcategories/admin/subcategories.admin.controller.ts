8b0f4ae2919859b983391776222e4da163a160e5
Vito Drofenik <vito@Vitos-MBP.localdomain>
2024-06-24 09:24:37 +0200

    fixed renamed path prefixes

--- src/data-api/subcategories/admin/subcategories.admin.controller.ts
+++ src/data-api/subcategories/admin/subcategories.admin.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
-import { ADMIN_API_V1_PATH } from '~common/http/http.constant';
+import { API_V1_ADMIN_PATH } from '~common/http/http.constant';
 import { CreateSubcategoryAdminBodyDto } from './dto/create-subcategory.admin.body.dto';
 import { SubcategoryAdminResDto } from './dto/subcategory.admin.res.dto';
 
 @ApiTags('subcategories')
-@Controller(ADMIN_API_V1_PATH + '/subcategories')
+@Controller(API_V1_ADMIN_PATH + '/subcategories')
 export class SubcategoriesAdminController {
     constructor() {}
 
