c99ff13ae62842666dd76f1be5f8d3b23f58832b
njokpro <anej.muzic@gmail.com>
2024-06-24 10:10:41 +0200

    feat: add apiProperty examples

--- src/data-api/teams/admin/dto/create-team.admin.body.dto.ts
+++ src/data-api/teams/admin/dto/create-team.admin.body.dto.ts
@@ -2,11 +2,14 @@ import { IsString, IsOptional } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateTeamAdminBodyDto {
-    @ApiProperty({ description: 'Team name' })
+    @ApiProperty({ description: 'Team name', example: 'Team A' })
     @IsString()
     readonly name: string;
 
-    @ApiProperty({ description: 'Team description' })
+    @ApiProperty({
+        description: 'Team description',
+        example: 'The best team in the world',
+    })
     @IsOptional()
     @IsString()
     readonly description: string;
--- src/data-api/teams/user/dto/invite-user.user.body.dto.ts
+++ src/data-api/teams/user/dto/invite-user.user.body.dto.ts
@@ -2,7 +2,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 
 export class InviteUserBodyDto {
-    @ApiProperty({ description: 'User email' })
+    @ApiProperty({
+        description: 'User email',
+        example: 'janez.novak@gmail.com',
+    })
     @IsEmail()
     readonly email: string;
 }
