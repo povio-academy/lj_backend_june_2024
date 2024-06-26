15658760f302b82b28848167acd67d2f917444b8
Vito Drofenik <vito@Vitos-MBP.localdomain>
2024-06-26 10:06:25 +0200

    implemented paging on categories, subcategories and recurring transactionw

--- src/data-api/subcategories/user/subcategories.user.controller.ts
+++ src/data-api/subcategories/user/subcategories.user.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { API_V1_USER_PATH } from '~common/http/http.constant';
 import { SubcategoryUserResDto } from './dto/subcategory.user.res.dto';
+import { PagedReqDto } from '~data-api/common/dto/paged.req.dto';
+import { PagedResDto } from '~data-api/common/dto/paged.res.dto';
 
-@ApiTags('subcategories')
+@ApiTags('Subcategories')
 @Controller(API_V1_USER_PATH + '/subcategories')
 export class SubcategoriesUserController {
     constructor() {}
@@ -18,15 +20,38 @@ export class SubcategoriesUserController {
 
     @ApiOperation({ summary: 'Get all subcategories' })
     @Get()
-    async getSubcategories(): Promise<SubcategoryUserResDto[]> {
-        return [new SubcategoryUserResDto('id', 'name', 'categoryId')];
+    async getSubcategories(
+        @Query() paging: PagedReqDto,
+    ): Promise<PagedResDto<SubcategoryUserResDto>> {
+        return {
+            data: [
+                new SubcategoryUserResDto('id', 'name', 'categoryId'),
+                new SubcategoryUserResDto('id2', 'name2', 'categoryId2'),
+            ],
+            pagingInfo: {
+                page: 1,
+                pageSize: 10,
+                total: 2,
+            },
+        };
     }
 
     @ApiOperation({ summary: 'Get subcategories of a specific category' })
     @Get('category/:categoryId')
     async getSubcategoriesByCategory(
         @Param('categoryId') categoryId: string,
-    ): Promise<SubcategoryUserResDto[]> {
-        return [new SubcategoryUserResDto('id', 'name', 'categoryId')];
+        @Query() paging: PagedReqDto,
+    ): Promise<PagedResDto<SubcategoryUserResDto>> {
+        return {
+            data: [
+                new SubcategoryUserResDto('id', 'name', 'categoryId'),
+                new SubcategoryUserResDto('id2', 'name2', 'categoryId'),
+            ],
+            pagingInfo: {
+                page: 1,
+                pageSize: 10,
+                total: 2,
+            },
+        };
     }
 }
