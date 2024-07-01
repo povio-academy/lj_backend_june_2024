c5540e2329f4e9edb786e75c28e7df7b350e2deb
eva <eva.pincin@gmail.com>
2024-07-01 16:32:41 +0200

    update user test

--- test/test-suites/app/data-api/update-user-admin.e2e-spec.ts
+++ test/test-suites/app/data-api/update-user-admin.e2e-spec.ts
@@ -1,5 +1,10 @@
 import { INestApplication, UnauthorizedException } from '@nestjs/common';
-import { newUserFixture } from '@test/fixtures/user.fixture';
+import {
+    newUserFixture,
+    newUserFixturePassword,
+    updateUserFixture,
+    updateUserRoleFixture,
+} from '@test/fixtures/user.fixture';
 import {
     createTestingApp,
     startTestingApp,
@@ -17,6 +22,7 @@ import { AuthModule } from '~modules/auth/auth.module';
 import { CreateJwtTokenUseCase } from '~modules/auth/use-cases/create-jwt-token.use-case';
 import { after, before } from 'node:test';
 import { UsersAdminModule } from '~data-api/users/admin/users.admin.module';
+import e from 'express';
 
 describe('update user - admin (e2e)', () => {
     let app: INestApplication;
@@ -25,49 +31,52 @@ describe('update user - admin (e2e)', () => {
 
     beforeAll(async () => {
         const module = await createTestingApp({
-            imports: [UsersAdminModule],
+            imports: [UserModule, AuthModule],
         });
 
         app = await startTestingApp(module);
+        adminAccessToken = await newJwtAdminTokenFixture(app);
+        userAccessToken = await newJwtUserTokenFixture(app);
     });
-    afterAll(async () => {});
 
-    // beforeAll(async () => {
-    //     const module = await createTestingApp({
-    //         imports: [UserModule, AuthModule],
-    //     });
-
-    //     app = await startTestingApp(module);
-    //     const createJwtTokenUseCase = app.get(CreateJwtTokenUseCase);
-    //     adminAccessToken = await newJwtAdminTokenFixture(app);
-    //     userAccessToken = await newJwtUserTokenFixture(app);
-    // });
-
-    // afterAll(async () => {
-    //     await stopTestingApp(app);
-    // });
+    afterAll(async () => {
+        await stopTestingApp(app);
+    });
 
     it('should not create user - JWT token not valid', async () => {
         const exsistingUser = await newUserFixture(app);
-        exsistingUser.role = UserRole.USER;
 
+        const updatedUser = await updateUserRoleFixture(
+            app,
+            exsistingUser,
+            UserRole.ADMIN,
+        );
+
+        console.log('/' + API_V1_ADMIN_PATH + '/users/' + updatedUser.id);
+        console.log(exsistingUser);
+        console.log(updatedUser);
         const response = await request(app.getHttpServer())
-            .patch(`/${API_V1_ADMIN_PATH}/users/${exsistingUser.id}`)
+            .patch('/' + API_V1_ADMIN_PATH + '/users/' + updatedUser.id)
             .set('Authorization', 'Bearer invalid-token');
 
-        expect(response).toBe(UnauthorizedException);
+        expect(response.body.message).toBe('Unauthorized');
+        expect(response.statusCode).toBe(401);
     });
 
-    // it('should not create user - user not authorized as admin, just as user', async () => {
-    //     const exsistingUser = await newUserFixture(app);
-    //     exsistingUser.role = UserRole.USER;
+    it('should not create user - user not authorized as admin, just as user', async () => {
+        const exsistingUser = await newUserFixture(app);
+        const updatedUser = await updateUserRoleFixture(
+            app,
+            exsistingUser,
+            UserRole.USER,
+        );
 
-    //     const response = await request(app.getHttpServer()).patch(
-    //         `/${API_V1_ADMIN_PATH}/users/${exsistingUser.id}`,
-    //     );
+        const response = await request(app.getHttpServer())
+            .patch('/' + API_V1_ADMIN_PATH + '/users/' + updatedUser.id + '/')
+            .set('Authorization', 'Bearer "' + userAccessToken + '"');
 
-    //     expect(response).toBe(ForbiddenException);
-    // });
+        expect(response.statusCode).toBe(401);
+    });
 
     // test.each`
     //     caseDesc        | payload
