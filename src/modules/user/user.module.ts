407e0b125d360ffbb295d9233afcecf38b4d3d45
eva <eva.pincin@gmail.com>
2024-07-01 09:32:58 +0200

    merged

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,10 @@
-import { Get, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { CreateUserUseCase } from './use-cases/create-user.use-case';
 import { InviteUserUseCase } from './use-cases/invite-user.use-case';
-import { DbModule } from '~db/db.module';
-import { GetUserUseCase } from './use-cases/get-user.use-case';
 import { NotificationModule } from '~modules/notification/notification.module';
 import { UpdateUserAdminUseCase } from './use-cases/update-user-admin.use-case';
+import { DbModule } from '~db/db.module';
+import { GetUserUseCase } from './use-cases/get-user.use-case';
 @Module({
     imports: [DbModule, NotificationModule],
     exports: [CreateUserUseCase, UpdateUserAdminUseCase],
